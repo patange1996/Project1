b097e6c90cae31982efb2ec0ce71951ae668468c
patange1996 <61934450+patange1996@users.noreply.github.com>
2023-12-22 20:32:55 +0530

    Update Mediamarkt final code.py

--- Mediamarkt final code.py
+++ Mediamarkt final code.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup as soup
-from urllib.request import urlopen as ureq
+import urllib.request
 import csv
 
 Links_csv = "Links.csv"
@@ -10,49 +10,56 @@ f.write(headers)
 nxt_link= ""
 url = input("Link : ")
 
-uclient = ureq(url)
-page_html=uclient.read()
-uclient.close()
-page_soup=soup(page_html,"html.parser")
-containers=page_soup.find_all("div" , {"class" : "product-wrapper"})
+req = urllib.request.Request(
+    url,
+    data=None,
+    headers={
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0'
+    }
+)
+op = urllib.request.urlopen(req)
+# uclient = ureq(url)
+page_html = op.read().decode('utf-8')
+# uclient.close()
+page_soup = soup(page_html, "html.parser")
+containers = page_soup.find_all("div", {"data-test": "mms-product-card"})
 
-x=int(len(containers))
-y=x
+x = int(len(containers))
+y = x
 for i in range(x):
     container = containers[i]
-    link = container.find("div" , {"class" : "content"})
-    links_found = link.h2.a["href"]
+    links_found = container.div.div.a["href"]
     f.write(links_found + "\n")
 
-track = 1
-print("Done")
-nxt = page_soup.find("li" , {"rel" : "next"})
-if nxt is None:
-    nxt = "none"
-
-
-while len(nxt) == 3:
-    nxt_link = "https:" + str(nxt.a["href"].strip())
-    uclient1 = ureq(nxt_link)
-    page_html = uclient1.read()
-    uclient1.close()
-    page_soup = soup(page_html, "html.parser")
-    containers = page_soup.find_all("div", {"class": "product-wrapper"})
-
-    x = int(len(containers))
-    y=y+x
-    for i in range(x):
-        container = containers[i]
-        link = container.find("div", {"class": "content"})
-        links_found = link.h2.a["href"]
-        f.write(links_found + "\n")
-
-    track=track+1
-    print("Done")
-    nxt = page_soup.find("li", {"rel": "next"})
-    if nxt is None:
-        nxt="none"
+#### The pagination logic is expired as the site included a button #####
+# track = 1
+# print("Done")
+# nxt = page_soup.find("li", {"rel": "next"})
+# if nxt is None:
+#     nxt = "none"
+#
+#
+# while len(nxt) == 3:
+#     nxt_link = "https:" + str(nxt.a["href"].strip())
+#     uclient1 = ureq(nxt_link)
+#     page_html = uclient1.read()
+#     uclient1.close()
+#     page_soup = soup(page_html, "html.parser")
+#     containers = page_soup.find_all("div", {"class": "product-wrapper"})
+#
+#     x = int(len(containers))
+#     y=y+x
+#     for i in range(x):
+#         container = containers[i]
+#         link = container.find("div", {"class": "content"})
+#         links_found = link.h2.a["href"]
+#         f.write(links_found + "\n")
+#
+#     track=track+1
+#     print("Done")
+#     nxt = page_soup.find("li", {"rel": "next"})
+#     if nxt is None:
+#         nxt="none"
 f.close()
-print(str(track) + " Pages scanned")
-print(str(y) + " Links found")
-
+# print(str(track) + " Pages scanned")
+# print(str(y) + " Links found")
